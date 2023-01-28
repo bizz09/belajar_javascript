dbdffbeeacaab0fc26786349bdbe39357806fd02
unknown <wanjuvada@gmail.com>
2023-01-28 11:10:42 +0800

    update

---
+++ control_flow/script.js
@@ -0,0 +1,12 @@
+// alert('Mulai');
+// for(var i = 0; i < 5; i++){
+//     alert('hallo bizz')
+// }
+// alert('selesai')
+
+var angka = prompt('Masukkan angka : ')
+if (angka % 2 === 0) {
+    alert(angka + ' adala angka GENAP')
+} else {
+    alert(angka + ' adalah angka GANJIL')
+}
---
+++ while/script.js
@@ -0,0 +1,12 @@
+var ulang = true
+while (ulang) {
+    console.log('hallo biz')
+    ulang = confirm('lagii?')
+}
+
+var nilaiAwal = 1;
+while (nilaiAwal <= 10) {
+    console.log('Hallo izzu ' + nilaiAwal + 'x');
+    nilaiAwal++;
+}
+
